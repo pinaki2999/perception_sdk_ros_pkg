643f9d73103932a04dd7a01d0a909e2fabd5a8e6
Pinaki Banerjee <pinaki2999@gmail.com>
2011-10-11 13:34:20 +0200

    added class for kinect sesor grabbing

---
+++ lib/core/depthPerception/kinectDataGrabber.h
@@ -0,0 +1,21 @@
+/*
+ * kinectDataGrabber.h
+ *
+ *  Created on: Oct 11, 2011
+ *      Author: reon
+ */
+
+#ifndef KINECTDATAGRABBER_H_
+#define KINECTDATAGRABBER_H_
+
+namespace BRICS_3D {
+
+class kinectDataGrabber {
+public:
+	kinectDataGrabber();
+	virtual ~kinectDataGrabber();
+};
+
+}
+
+#endif /* KINECTDATAGRABBER_H_ */
