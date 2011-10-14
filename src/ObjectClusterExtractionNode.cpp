330e7f43b6e3408d6629d0e27fe667daaaeafccc
Pinaki Banerjee <pinaki2999@gmail.com>
2011-10-14 17:33:40 +0200

    added classes for object cluster extraction example

---
+++ src/ObjectClusterExtractionNode.cpp
@@ -0,0 +1,8 @@
+/*
+ * ObjectClusterExtractionNode.cpp
+ *
+ *  Created on: Oct 14, 2011
+ *      Author: Pinaki Sunil Banerjee
+ */
+
+
---
+++ src/examples/EuclideanClusterExtraction.cpp
@@ -0,0 +1,21 @@
+/*
+ * EuclideanClusterExtraction.cpp
+ *
+ *  Created on: Oct 14, 2011
+ *      Author: Pinaki Sunil Banerjee
+ */
+
+#include "EuclideanClusterExtraction.h"
+
+namespace BRICS_3D {
+
+EuclideanClusterExtraction::EuclideanClusterExtraction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EuclideanClusterExtraction::~EuclideanClusterExtraction() {
+	// TODO Auto-generated destructor stub
+}
+
+}
