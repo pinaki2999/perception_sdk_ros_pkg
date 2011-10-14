614cc9703adc1aa071daae1640d04ec5f2d831fe
Pinaki Banerjee <pinaki2999@gmail.com>
2011-10-14 15:34:12 +0200

    added class for euclidean cluster extraction wrapper for PCL

---
+++ lib/core/segmentation/EuclideanClusterExtraction.cpp
@@ -0,0 +1,21 @@
+/*
+ * EuclideanClusterExtraction.cpp
+ *
+ *  Created on: Oct 14, 2011
+ *      Author: Pinaki Sunil Banerjee
+ */
+
+#include "EuclideanClusterExtraction.h"
+
+namespace BRICS_3D {
+
+EucledeanClusterExtraction::EucledeanClusterExtraction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EucledeanClusterExtraction::~EucledeanClusterExtraction() {
+	// TODO Auto-generated destructor stub
+}
+
+}
---
+++ lib/core/segmentation/EuclideanClusterExtraction.h
@@ -0,0 +1,26 @@
+/*
+ * EuclideanClusterExtraction.h
+ *
+ *  Created on: Oct 14, 2011
+ *      Author: Pinaki Sunil Banerjee
+ */
+
+#ifndef EUCLEDEANCLUSTEREXTRACTION_H_
+#define EUCLEDEANCLUSTEREXTRACTION_H_
+
+
+
+namespace BRICS_3D {
+
+/**
+ * The class provides a wrapper for Eucledean Cluster Extraction in PCL
+ */
+class EucledeanClusterExtraction {
+public:
+	EucledeanClusterExtraction();
+	virtual ~EucledeanClusterExtraction();
+};
+
+}
+
+#endif /* EUCLEDEANCLUSTEREXTRACTION_H_ */
