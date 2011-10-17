52ff15bc8c9ae27eb1ef7c2cb6d075745080d301
Pinaki Banerjee <pinaki2999@gmail.com>
2011-10-17 16:59:50 +0200

    modified filename for object cluster extraction example

---
+++ src/examples/EuclideanClusterExtractor.cpp
@@ -0,0 +1,84 @@
+/*
+ * EuclideanClusterExtractor.cpp
+ *
+ *  Created on: Oct 14, 2011
+ *      Author: Pinaki Sunil Banerjee
+ */
+
+#include "EuclideanClusterExtractor.h"
+
+namespace BRICS_3D {
+
+EuclideanClusterExtractor::EuclideanClusterExtractor() {}
+
+ros::Publisher *EuclideanClusterExtractor::getExtractedClusterPublisher() const
+{
+	return extractedClusterPublisher;
+}
+
+int EuclideanClusterExtractor::getMaxNoOfObjects() const
+{
+	return maxNoOfObjects;
+}
+
+void EuclideanClusterExtractor::setMaxNoOfObjects(int maxNoOfObjects)
+{
+	this->maxNoOfObjects = maxNoOfObjects;
+}
+
+void EuclideanClusterExtractor::setExtractedClusterPublisher(ros::Publisher *extractedClusterPublisher){
+	this->extractedClusterPublisher = extractedClusterPublisher;
+}
+
+EuclideanClusterExtractor::~EuclideanClusterExtractor() {}
+
+
+void EuclideanClusterExtractor::kinectCloudCallback(const sensor_msgs::PointCloud2 &cloud){
+	//printf("Nothing is implemented yet!!!! \n");
+
+	//ToDo assert if every thing is initialized or not
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_xyz_rgb_ptr(new pcl::PointCloud<pcl::PointXYZRGB>());
+
+    BRICS_3D::PointCloud3D *in_cloud = new BRICS_3D::PointCloud3D();
+    std::vector<BRICS_3D::PointCloud3D*> extracted_clusters;
+
+
+    //Transform sensor_msgs::PointCloud2 msg to pcl::PointCloud
+    pcl::fromROSMsg (cloud, *cloud_xyz_rgb_ptr);
+    //printf("[ECExtraction]Input cloud Size: %d \n", cloud_xyz_rgb_ptr->points.size());
+    // cast PCL to BRICS_3D type
+    pclTypecaster.convertToBRICS3DDataType(cloud_xyz_rgb_ptr, in_cloud);
+    //printf("[ECExtraction]Converted cloud Size: %d \n", in_cloud->getSize());
+    //extract the clusters
+    euclideanClusterExtractor.extractClusters(in_cloud, &extracted_clusters);
+    //printf("Number of clusters found = %d \n", extracted_clusters.size());
+
+    //Publish the extracted clusters
+	pcl::PointCloud<pcl::PointXYZ>::Ptr tempCloud(new pcl::PointCloud<pcl::PointXYZ>());
+    for (unsigned int i = 0; i < extracted_clusters.size(); i++){
+    	pclTypecaster.convertToPCLDataType(tempCloud, extracted_clusters[i]);
+
+    	tempCloud->header.frame_id = "openni_rgb_optical_frame";
+    	extractedClusterPublisher[i].publish(*tempCloud);
+    }
+
+    tempCloud.reset();
+    delete(in_cloud);
+    extracted_clusters.clear();
+
+}
+
+void EuclideanClusterExtractor::initializeExtractor(int maxNoObjects,
+		ros::Publisher *extractedClusterPublisher, int minClusterSize, int maxClusterSize,
+																		float clusterTolerance){
+
+	setMinClusterSize(minClusterSize);
+	setMaxClusterSize(maxClusterSize);
+	setClusterTolerance(clusterTolerance);
+	setMaxNoOfObjects(maxNoObjects);
+	setExtractedClusterPublisher(extractedClusterPublisher);
+
+}
+
+}
---
+++ src/examples/EuclideanClusterExtractor.h
@@ -0,0 +1,81 @@
+/*
+ * EuclideanClusterExtractor.h
+ *
+ *  Created on: Oct 14, 2011
+ *      Author: Pinaki Sunil Banerjee
+ */
+
+#ifndef EUCLIDEANCLUSTEREXTRACTION_H_
+#define EUCLIDEANCLUSTEREXTRACTION_H_
+
+#include "sensor_msgs/PointCloud2.h"
+#include "sensor_msgs/point_cloud_conversion.h"
+#include "ros/publisher.h"
+#include "pcl_ros/point_cloud.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+
+#include "segmentation/EuclideanClusterExtraction.h"
+#include "util/PCLTypecaster.h"
+
+//Todo add the comments
+namespace BRICS_3D {
+
+class EuclideanClusterExtractor {
+private:
+
+	ros::Publisher *extractedClusterPublisher;
+
+	BRICS_3D::PCLTypecaster pclTypecaster;
+	BRICS_3D::EucledeanClusterExtraction euclideanClusterExtractor;
+
+	int maxNoOfObjects;
+
+public:
+	EuclideanClusterExtractor();
+	virtual ~EuclideanClusterExtractor();
+
+	void kinectCloudCallback(const sensor_msgs::PointCloud2 &cloud);
+
+
+    void setExtractedClusterPublisher(ros::Publisher *extractedClusterPublisher);
+    ros::Publisher *getExtractedClusterPublisher() const;
+
+    int getMaxNoOfObjects() const;
+    void setMaxNoOfObjects(int maxNoOfObjects);
+
+    inline void setMinClusterSize(int size){
+    	this->euclideanClusterExtractor.setMinClusterSize(size);
+    }
+
+
+    inline int getMinCLusterSize(){
+    	return this->euclideanClusterExtractor.getMinClusterSize();
+    }
+
+    inline void setMaxClusterSize(int size){
+        	this->euclideanClusterExtractor.setMaxClusterSize(size);
+        }
+
+    inline int getMaxClusterSize(){
+    	return this->euclideanClusterExtractor.getMaxClusterSize();
+    }
+
+    inline void setClusterTolerance(float tolerance){
+        	this->euclideanClusterExtractor.setClusterTolerance(tolerance);
+        }
+
+    inline float getClusterTolerance(){
+     return	this->euclideanClusterExtractor.getClusterTolerance();
+    }
+
+    void initializeExtractor(int maxNoObjects, ros::Publisher *extractedClusterPublisher,
+    		int minClusterSize, int maxClusterSize, float clusterTolerance);
+
+};
+
+}
+
+#endif /* EUCLIDEANCLUSTEREXTRACTION_H_ */
