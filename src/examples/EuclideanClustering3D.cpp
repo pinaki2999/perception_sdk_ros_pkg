cb885e6f4b93c2980d6a3117bfa77c730cb17db2
Pinaki Banerjee <pinaki2999@gmail.com>
2011-10-24 11:50:57 +0200

    added class for PCL-independent euclidean clustering

---
+++ src/examples/EuclideanClustering3D.cpp
@@ -0,0 +1,33 @@
+/*
+ * EuclideanClustering3D.cpp
+ *
+ *  Created on: Oct 24, 2011
+ *      Author: Pinaki Sunil Banerjee
+ */
+
+#include "EuclideanClustering3D.h"
+
+namespace BRICS_3D {
+
+EuclideanClustering3D::EuclideanClustering3D() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EuclideanClustering3D::~EuclideanClustering3D() {
+	// TODO Auto-generated destructor stub
+}
+
+void EuclideanClustering3D::extractClusters(BRICS_3D::PointCloud3D *inCloud,
+		std::vector<BRICS_3D::PointCloud3D*> *extractedClusters){
+	printf("Nothing is implemented \n");
+
+}
+
+void EuclideanClustering3D::extractClusters(BRICS_3D::ColoredPointCloud3D *inCloud,
+		std::vector<BRICS_3D::ColoredPointCloud3D*> *extractedClusters){
+	printf("Nothing is implemented \n");
+
+}
+
+}
